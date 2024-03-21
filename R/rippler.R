73b39d2719b31bf17c612e434ef4ee83e6243601
lpiep <loganap@uw.edu>
2024-03-21 09:37:20 -0700

    mvp

--- R/rippler.R
+++ R/rippler.R
@@ -1,25 +1,27 @@
 
-#' Pull Data from Ripple
+#' Pull Data from Ripple Science API
 #'
-#' @param url
-#' @param study_id
-#' @param variables implement me
-#' @param output_file
+#' @param url URL of your Ripple Instance, by default pulled from `RIPPLER_RIPPLE_URL` environment variable.
+#' @param study_id Study ID to pull from, by default pulled from `RIPPLER_RIPPLE_STUDY_ID` environment variable.
+#' @param variables (optional) a vector of variables to pull, by default the complete set of standard and custom variables
+#' @param output_file (optional) a CSV to write data to
 #'
-#' @return
+#' @return a data frame containing the data from Ripple Science API (invisibly, if `output_file` is specified)
 #' @export
 #'
-#' @examples
 ripple_download <- function(
-    url = Sys.getenv('rippler.ripple_url'),
-    study_id = Sys.getenv('rippler.study_id'),
-    variables = standard_vars,
+    url = Sys.getenv('RIPPLER_RIPPLE_URL'),
+    study_id = Sys.getenv('RIPPLER_RIPPLE_STUDY_ID'),
+    variables = c(standard_vars, custom_vars),
     output_file = NULL
   ){
   req <- httr2::request(url) |>
     httr2::req_url_path('/v1/export/') |>
-    httr2::req_auth_basic(Sys.getenv('rippler.ripple_user', unset = NA), Sys.getenv('rippler.ripple_pass', unset = NA)) |>
-    httr2::req_method('POST') |>
+  	httr2::req_auth_basic(
+  		ifelse(Sys.getenv('RIPPLER_RIPPLE_USER') == '', getPass::getPass('Ripple Username', forcemask = FALSE), Sys.getenv('RIPPLER_RIPPLE_USER')),
+  		ifelse(Sys.getenv('RIPPLER_RIPPLE_PASS') == '', getPass::getPass('Ripple Password'), Sys.getenv('RIPPLER_RIPPLE_PASS'))
+  	) |>
+  	httr2::req_method('POST') |>
     httr2::req_body_raw(
       glue::glue(
         'export-type={study_id}',
@@ -40,31 +42,61 @@ ripple_download <- function(
   	return(invisible(output_file))
   }
   # Read into R
-  dat <- readr::read_csv(text = httr2::resp_body_string(resp), stringsAsFactors = FALSE, check.names = FALSE)
+  dat <- read.csv(text = httr2::resp_body_string(resp), stringsAsFactors = FALSE, check.names = FALSE)
   return(dat)
 }
 
+#' Push data to Ripple Science API
+#'
+#' @param url URL of your Ripple Instance, by default pulled from `RIPPLER_RIPPLE_URL` environment variable.
+#' @param study_id Study ID to pull from, by default pulled from `RIPPLER_RIPPLE_STUDY_ID` environment variable.
+#' @param dat a data frame to push to Ripple (required if `file` not set)
+#' @param file an xlsx file to push to Ripple (required if `dat` is not set)
+#' @param update update type. One of `'all'` (update all participants, the default),
+#' `'nocontact'` (do not update any contact info), or
+#' `'noexisting'`(do not update any existing participants).
+#'
+#' @return value returned by `httr2::req_perform` on upload
+#' @export
+#'
 ripple_upload <- function(
+		url = Sys.getenv('RIPPLER_RIPPLE_URL'),
+		study_id = Sys.getenv('RIPPLER_RIPPLE_STUDY_ID'),
     dat = NULL,
-    file = NULL
+    file = NULL,
+    update = c('all', 'nocontact', 'noexisting') # default all
   ){
-  assertthat::assert_that(xor(is.null(dat), is.null(file)), msg = 'One (and not both) of `dat`, `file` must be provided.')
+	assertthat::assert_that(xor(is.null(dat), is.null(file)), msg = 'One (and not both) of `dat`, `file` must be provided.')
   assertthat::assert_that(!is.null(dat) || assertthat::has_extension(file, '.xlsx'), msg = '`file` should be an xlsx file.')
+	update <- rlang::arg_match(update)
+
+	if(is.null(file)){ # format and write temp file if given data frame
+		file <- tempfile(fileext = '.xlsx')
+		dat$importType <- study_id
+		writexl::write_xlsx(dat, file)
+	}
   req <- httr2::request(url) |>
     httr2::req_url_path('/v1/import/') |>
-    httr2::req_auth_basic(Sys.getenv('rippler.ripple_user', unset = NA), Sys.getenv('rippler.ripple_pass', unset = NA)) |>
+    httr2::req_auth_basic(Sys.getenv('RIPPLER_RIPPLE_USER', unset = getPass::getPass('Ripple Username', forcemask = FALSE)), Sys.getenv('RIPPLER_RIPPLE_PASS', unset = getPass::getPass('Ripple Password'))) |>
     httr2::req_method('POST') |>
     httr2::req_url_query(
       importtype = study_id,
-      updateoption = 'all'
+      updateoption = update
     ) |>
     httr2::req_headers(
       `Content-Type` = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    	#`Content-Type` = 'text/csv',
       `Connection` = 'keep-alive',
       `Keep-Alive` = 'timeout=360, max=1000'
     ) |>
     httr2::req_body_file(file) |>
-    httr2::req_error(body = function(resp) resp_body_string(resp)) # Include body of response in error
-
-  httr2::req_perform(req)
+    httr2::req_error(body = function(resp) httr2::resp_body_string(resp)) # Include body of response in error
+	tryCatch(
+		{
+  		httr2::req_perform(req)
+		},
+		finally = if(!is.null(dat)) file.remove(file) # clean up temp files
+	)
 }
+
+
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# r-ripple-client
-Communicate with Ripple Science from R
+# rippler
+
+## Communicate with Ripple Science from R
+
+_Note: this package is in early stages and in progress!_ 
+
+### To Do
+
+* [ ] Implement config files for auth details and custom variables (currently custom vars are hardcoded in `zzz.R` and auth details are manually added as env vars. 
+
+* [ ] Proper Readme
+
+* [ ] Support exporting xlsx files, importing csv files 
+
+* [ ] Allow passing options to final `httr:req_perform` command
+
+* [ ] Allow filtering exports on date
+
+
+### Authentication 
+
+Add the following to your ~/.Renviron file, or provide them whenever calling the functions. If not set here,
+Rippler will ask for your Ripple username and password in a dialog box. 
+
+```
+RIPPLER_RIPPLE_URL=https://myripplesite.ripplescience.com
+RIPPLER_STUDY_ID=abcdefghijk123
+RIPPLER_RIPPLE_USER=myemail@mycompany.com
+RIPPLER_RIPPLE_PASS=yourPassword123
+```
+
+### Ripple API Details
+
+https://support.ripplescience.com/hc/en-us/categories/21426318229389-Application-Programming-Interface-API
+
+### Gripes
+
+* It would be really nice if the exported file were in the same format as what's needed for importing. E.g. why no importType/ field? Why does sex export in lower case but needs title case for import?
+
+* A complete API documentation would be nice
